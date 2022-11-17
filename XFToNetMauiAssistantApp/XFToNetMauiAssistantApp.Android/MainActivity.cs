2cdb5fb1fb2099d5df9dc30dd101777f292a7af3
Estivenson Ortega Villar <eortegav@intergrupo.com>
2022-11-17 09:55:41 -0500

    Actualización en la clase MainActivity para eliminar dos errores

--- XFToNetMauiAssistantApp/XFToNetMauiAssistantApp.Android/MainActivity.cs
+++ XFToNetMauiAssistantApp/XFToNetMauiAssistantApp.Android/MainActivity.cs
@@ -8,21 +8,11 @@ using Android.OS;
 namespace XFToNetMauiAssistantApp.Droid
 {
     [Activity(Label = "XFToNetMauiAssistantApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : MauiAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
-
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+            base.OnCreate(savedInstanceState); 
+        } 
     }
 }
