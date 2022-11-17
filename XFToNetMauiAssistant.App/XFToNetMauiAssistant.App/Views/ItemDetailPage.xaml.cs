13ab3b72b041903a1ce518f3962d2368adc64da5
Estivenson Ortega Villar <eortegav@intergrupo.com>
2022-11-16 22:33:55 -0500

    Actualización de los NuGets

---
+++ XFToNetMauiAssistant.App/XFToNetMauiAssistant.App/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XFToNetMauiAssistant.App.ViewModels;
+
+namespace XFToNetMauiAssistant.App.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
